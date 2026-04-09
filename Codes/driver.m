88298a722786e616ec59c45f288b011153b24cf7
Zeon Chen <156951866+TangleMonster@users.noreply.github.com>
2026-04-09 13:10:04 +0200

    Finished question 11 Finished question 12: - prove the convergence in two parts- time and space
    Removed the files from the template that weren't needed.

--- Codes/driver.m
+++ Codes/driver.m
@@ -31,7 +31,7 @@ legend('exact', 'approximation');
 % set n list that satisfies h half-decreasing, so according the O(h^2), the
 % error should be reduced to 1/4
 n_list = [4,9,19,39,79,159,319,639];
-errors = zeros(size(n_list));
+spatial_errors = zeros(size(n_list));
 % expected_ratio = 1/4;
 
 for i = 1:length(n_list)
@@ -53,18 +53,18 @@ for i = 1:length(n_list)
     exact = -(pi^2/4)*cos(pi*x/2);
 
     % max error
-    errors(i) = max(abs(V-exact));
+    spatial_errors(i) = max(abs(V-exact));
 end
 
 h_list = 2./(n_list+1);
-error_ratio = errors(1:end-1) ./ errors(2:end);
+error_ratio = spatial_errors(1:end-1) ./ spatial_errors(2:end);
 expected_ratio = (h_list(1:end-1)./h_list(2:end)).^2;
 
 % plot
 figure;
-loglog(h_list, errors, 'bo-', 'LineWidth', 2);
+loglog(h_list, spatial_errors, 'bo-', 'LineWidth', 2);
 hold on;
-loglog(h_list, errors(1)* (h_list/h_list(1)).^2, 'r--', 'LineWidth', 2);
+loglog(h_list, spatial_errors(1)* (h_list/h_list(1)).^2, 'r--', 'LineWidth', 2);
 xlabel('h');
 ylabel('Maximum Error');
 legend('Numerical Error', 'O(h^2) Reference', 'Location', 'best');
@@ -73,9 +73,9 @@ legend('Numerical Error', 'O(h^2) Reference', 'Location', 'best');
 fprintf('h \t\t Error \t\t Error Ratio \t Excepted Ratio \n');
 for i = 1:length(n_list)
     if i < length(n_list)
-        fprintf('%.4f \t\t %.2e \t\t %.2f \t\t %.2f \n', h_list(i), errors(i), error_ratio(i),  expected_ratio(i));
+        fprintf('%.4f \t\t %.2e \t\t %.2f \t\t %.2f \n', h_list(i), spatial_errors(i), error_ratio(i),  expected_ratio(i));
     else
-        fprintf('%.4f \t\t %.2e \t\t - \t\t - \n', h_list(i), errors(i));
+        fprintf('%.4f \t\t %.2e \t\t - \t\t - \n', h_list(i), spatial_errors(i));
     end
 end
 
@@ -224,10 +224,187 @@ plot(tHist, v_exact, 'ro', 'MarkerSize', 3);
 legend('Forward Eular', 'Exact');
 title('v(t)')
 %% Question 9
-
+% Pen-and-paper proof is written in LaTeX report.
 
 %% Question 10
+% Pen-and-paper proof is written in LaTeX report.
 
 %% Question 11
+% spatial grid
+n = 100; 
+h = 1 / (n+1);  % Spacing
+x = 0 + (1:n)' * h; % internal vector point (not including 0 and 1)
+x_full = (0: h :1)';
 
+% time grid
+T=1;
+nsteps = 100;
+tss = T / nsteps;
+t = (0:tss:T);
+
+% build matrix
+D = (diag(ones(n-1,1),1) + diag(-2*ones(n,1)) + diag(ones(n-1,1),-1)) * (1/h^2);
+
+% Using Crank Nicholson
+I = eye(n);
+B = I - (tss/2)*D;
+L = I + (tss/2)*D;
+
+% initial condition
+phi = sin(2*pi*x);
+U = phi;
+U_records = zeros(nsteps+1, n);
+U_records(1, :) = phi';
+
+for i = 1:nsteps
+    U = B \ (L*U);
+    U_records(i+1, :) = U';
+end
+
+% build the full matrix including the boundary points (0 and 1)
+U_full = zeros(nsteps+1, n+2);
+U_full(:, 1) = 0;  %left boundary
+U_full(:, end) = 0;  % right boundary
+U_full(:, 2:end-1) = U_records;
+
+[X, T_mesh] = meshgrid(x_full, t);
+U_exact = exp(-4*pi^2*T_mesh) .* sin(2*pi*X);
+
+% plot
+figure;
+surf(X, T_mesh, U_full, 'EdgeColor', 'none');
+xlabel('x');
+ylabel('t');
+zlabel('u(x,t)');
+title('Crank-Nicolson');
+
+figure;
+surf(X, T_mesh, U_exact, 'EdgeColor', 'none');
+xlabel('x');
+ylabel('t');
+zlabel('u(x,t)');
+title('Analystical one');
 %% Question 12
+%% initial fixed time step settings
+n_list = [5,10,20,40,80,160,320,640];
+T = 0.05;
+tss_fixed = 0.001;
+nsteps_fixed = round(T / tss_fixed);
+
+% pre_set
+h_list = zeros(length(n_list), 1);
+spatial_errors = zeros(length(n_list),1);
+
+for i = 1:length(n_list)
+    n = n_list(i);
+    h = 1/(n+1);
+    h_list(i) = h;
+
+    x = (1:n)' * h;
+
+    % build matrix
+    D = (diag(ones(n-1,1),1) + diag(-2*ones(n,1)) + diag(ones(n-1,1),-1)) * (1/h^2);
+    
+    % Using Crank Nicholson
+    I = eye(n);
+    B = I - (tss_fixed/2)*D;
+    L = I + (tss_fixed/2)*D;
+
+    % initial condition
+    phi = sin(2*pi*x);
+    U = phi;
+
+    % calcu the numerical solution for internal points at time t
+    for step = 1:nsteps_fixed
+        U = B \ (L*U);
+    end
+
+    % build the full matrix including the boundary points (0 and 1)
+    x_full = (0:h:1)';
+    U_full = zeros(1, n+2);
+    U_full(1) = 0; %left boundary
+    U_full(end) = 0; % right boundary
+    U_full(2:end-1) = U';
+
+    % excat soluction
+    U_exact = exp(-4*pi^2*T) .* sin(2*pi*x_full);
+    
+    % calcu the error
+    spatial_errors(i) = max(abs(U_full' - U_exact));
+end 
+
+% calcu the space convergence rates
+space_error_rates = log(spatial_errors(1:end-1) ./ spatial_errors(2:end)) ./ log(h_list(1:end-1) ./ h_list(2:end));
+fprintf('%.2f  ', space_error_rates);
+
+% plot
+figure;
+loglog(h_list, spatial_errors, 'bo-', 'LineWidth', 2);
+hold on;
+loglog(h_list, spatial_errors(1)*(h_list/h_list(1)).^2, 'r--');
+xlabel('h');
+ylabel('Error');
+title('Space Convergence: O(h^2)');
+legend('Numerical', 'O(h^2)', 'Location', 'best');
+grid on;
+
+%% initial fixed spatial step settings
+n_fixed = 500;
+h_fixed = 1 / (n_fixed+1);
+x_fixed = (1:n_fixed)' * h_fixed;
+
+% time step setting
+T = 0.05;
+tss_list = [0.1, 0.05, 0.025, 0.0125, 0.00625, 0.003125, 0.0014625];
+
+% initial condition
+phi_fixed = sin(2*pi*x_fixed);
+
+
+% pre-set
+time_errors = zeros(size(tss_list));
+
+% build matrix
+D_fixed = (diag(ones(n_fixed-1,1),1) + diag(-2*ones(n_fixed,1)) + diag(ones(n_fixed-1,1),-1)) * (1/h_fixed^2);
+
+for i = 1:length(tss_list)
+    tss = tss_list(i);
+    nsteps = ceil(T/tss); % Ensure that the number of steps is an integer
+
+    % Using Crank Nicholson
+    I = eye(n_fixed);
+    B = I - (tss/2)*D_fixed;
+    L = I + (tss/2)*D_fixed;
+    U = phi_fixed;
+
+    % calcu the numerical solution for internal points at time t
+    for step = 1:nsteps
+        U = B \ (L*U);
+    end
+
+    % excat soluction
+    U_exact = exp(-4*pi^2*T) .* sin(2*pi*x_fixed);
+
+    % time error calcu and record
+    time_errors(i) = max(abs(U - U_exact));
+end
+
+% calcu the time convergence rates
+time_error_rates = log(time_errors(1:end-1) ./ time_errors(2:end)) ./ log(tss_list(1:end-1) ./ tss_list(2:end));
+fprintf('%.2f  ', time_error_rates);
+
+% plot
+figure;
+loglog(tss_list, time_errors, 'bo-', 'LineWidth', 2);
+hold on;
+loglog(tss_list, time_errors(1)*(tss_list/tss_list(1)).^2, 'r--');
+xlabel('tss');
+ylabel('Error');
+title('Time Convergence: O(h^2)');
+legend('Numerical', 'O(h^2)', 'Location', 'best');
+grid on;
+
+%% 
+% To verify convergence on spatial and time, I separately fixed the time
+% step size and spatial step size. The final results show that the algorithm converges
+% both on space and time in $O(h^2)$ time.
